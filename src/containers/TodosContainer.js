0a060f08730a1754dd8f568d96c98d3c8c474fea
Lillian Chernin <l.smith1654@gmail.com>
2018-01-30 12:21:53 -0600

    finish sprint 1

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodosContainer extends Component {
+  render () {
+    return (
+      <div>
+        <h1>This is the todos container page!</h1>
+      </div>
+    )
+  }
+}
+
+export default TodosContainer
